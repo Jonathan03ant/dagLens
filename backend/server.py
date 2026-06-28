3f929eb03396ab025c1e1ea28be54279418b4b1a
Jonathan Antenanie <jonathan03ant@gmail.com>
2026-06-28 02:33:35 +0000

    [server] run_llc() with future todo lists

--- backend/server.py
+++ backend/server.py
@@ -1,8 +1,21 @@
+import os
+import subprocess
+import tempfile
+import glob
+import re
 from flask import Flask, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
+
+DAG_STAGE_FLAGS = {
+    'dag-combine1': '-view-dag-combine1-dags',    # After build, before first optimization pass
+    'legalize': '-view-legalize-dags',            # Before legalization (after dag-combine1)
+    'dag-combine2': '-view-dag-combine2-dags',    # Before second optimization pass (after legalization)
+    'isel': '-view-isel-dags',                    # Before instruction selection (after dag-combine2)
+    'sched': '-view-sched-dags'                   # Before scheduling (after instruction selection)
+}
 
 @app.route('/api/health', methods=['GET'])
 def health():
@@ -11,7 +24,36 @@ def health():
         'message': 'DagLens Python API is running'
     })
 
+def run_llc(ir_code: str, stage: str):
+    """
+    Generates graph .dot file from LLVM IR
+    llc path: user configured, or in PATH (TODO)
+    -march: default amdgcn, later user defined (TODO)
+    -mcpu: default gfx1101, later user defined (TODO)
+    """
 
+    with open('/tmp/input.ll', 'w') as f:
+        f.write(ir_code)
+
+    llc_path = "llc"
+    flag = DAG_STAGE_FLAGS[stage]
+
+    cmd = [
+        llc_path,
+        '-march=amdgcn',      # TODO: make configurable
+        '-mcpu=gfx1101',      # TODO: make configurable
+        '/tmp/input.ll',
+        flag,
+        '-o', '/dev/null'  # Don't generate assembly, just .dot
+    ]
+
+    subprocess.run(cmd, check=True)
+
+    dot_files = glob.glob('/tmp/dag.*.dot')
+    dot_files.sort(key=os.path.getmtime, reverse=True)
+    dot_file = dot_files[0]
+
+    return dot_file
 
 if __name__ == '__main__':
     print('DagLens server starting on http://localhost:8080')
